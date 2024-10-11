0ae830901208e4c53580ae1ca7bfe29941eebc26
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2024-10-11 16:54:51 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+$databaseHost = 'localhost:8080';
+$databaseName = 'to_do_list';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName);
